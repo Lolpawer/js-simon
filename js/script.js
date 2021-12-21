35906fb447ecbcc05f26760f42bd0a5c93d612f4
Lolpawer <dadu.petrosino@gmail.com>
2021-12-21 17:39:49 +0100

    done

--- js/script.js
+++ js/script.js
@@ -7,14 +7,27 @@ function hide () {
 }
 
 function simonSays () {
-    let promptContainer = document.getElementById('prompt');
+    let userInput = document.getElementById('prompt');
     for (let i = 0; i < 5; i++) {
-        promptContainer = parseInt(prompt("Inserisci un numero."));
+        userInput = parseInt(prompt("Inserisci un numero."));
+        if (numberArray.includes(userInput)) {
+            score.push(userInput);
+        }
+        let scoreOutput = document.getElementById('score');
+        scoreOutput.innerHTML = `Hai ottenuto ${score.length} punto/i.</br>`
+        let x = 0;
+        while (score.length > x) {
+            scoreOutput.innerHTML += `Numeri indovinati: </br>`;
+            for (x = 0; x < score.length; x++) {
+                scoreOutput.innerHTML += `${score[x]}</br>`;
+            }
+        }
     }
 }
 
 const numberOutput = document.getElementById('simon-says');
 let numberArray = [];
+let score = [];
 
 while (numberArray.length < 5) {
     let number = generateNumbers(1, 9999);
@@ -24,5 +37,5 @@ while (numberArray.length < 5) {
     }
 }
 
-setTimeout (hide, 5000);
-setTimeout (simonSays, 5100);
+setTimeout (hide, 30000);
+setTimeout (simonSays, 30100);
